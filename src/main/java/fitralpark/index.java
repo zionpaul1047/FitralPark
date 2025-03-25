51ad59624f00d1196ffe810f76778a27791cd540
zionpaul1047 <zionpaul1047@gmail.com>
2025-03-25 15:21:23 +0900

    zion
    다시커밋

---
+++ src/main/java/fitralpark/index.java
@@ -0,0 +1,23 @@
+package fitralpark;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/index.do")
+public class index extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		//index.java
+		
+
+		req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
+	}
+
+}
