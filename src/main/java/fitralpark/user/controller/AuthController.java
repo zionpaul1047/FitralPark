4755769e7d127e3cff9a0ba4121414d43b1ea9df
zionpaul1047 <zionpaul1047@gmail.com>
2025-03-27 12:03:37 +0900

    ..

--- src/main/java/fitralpark/user/controller/AuthController.java
+++ src/main/java/fitralpark/user/controller/AuthController.java
@@ -1,5 +1,24 @@
 package fitralpark.user.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 //로그인, 로그아웃, 회원가입, 비밀번호/아이디 찾기 컨트롤러
-public class AuthController {
+@WebServlet("/login.do")
+public class AuthController extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		//index.java
+		
+
+		req.getRequestDispatcher("/WEB-INF/views/user/login.jsp").forward(req, resp);
+	}
 
 }
