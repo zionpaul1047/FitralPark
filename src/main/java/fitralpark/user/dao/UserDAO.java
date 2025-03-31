7958bd2b209d76a699faa7f6338aa313ccfff300
zionpaul1047 <zionpaul1047@gmail.com>
2025-03-31 14:01:05 +0900

    .

--- src/main/java/fitralpark/user/dao/UserDAO.java
+++ src/main/java/fitralpark/user/dao/UserDAO.java
@@ -1,46 +1,38 @@
 package fitralpark.user.dao;
 
+import fitralpark.common.utils.DBUtil;
+import fitralpark.user.dto.UserDTO;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-import fitralpark.user.dto.UserDTO;
-import fitralpark.common.utils.DBUtil;
-
-//사용자 관련 DB CRUD 담당
 
 public class UserDAO {
 
-    public int insertMember(UserDTO user) {
-        int result = 0;
-
-        String sql = "INSERT INTO member (member_no, member_id, pw, member_nickname, member_name, personalnumber, "
-                   + "tel, email, address, member_pic, background_pic, allergy, fitness_score, community_score, "
-                   + "restrict_check, withdraw_check, mentor_check, admin_check, plan_public_check) "
+    public int insertMember(UserDTO dto) {
+        String sql = "INSERT INTO member (member_no, member_id, pw, member_nickname, member_name, personalnumber, tel, email, address, member_pic, background_pic, allergy, fitness_score, community_score, restrict_check, withdraw_check, mentor_check, admin_check, plan_public_check) "
                    + "VALUES (SEQMEMBER.NEXTVAL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 0, 0, 0, 0, 0, 0, 0)";
 
-        try (
-            Connection conn = DBUtil.getConnection(); // DB 연결
-            PreparedStatement pstmt = conn.prepareStatement(sql);
-        ) {
-            pstmt.setString(1, user.getMemberId());
-            pstmt.setString(2, user.getPw());
-            pstmt.setString(3, user.getMemberNickname());
-            pstmt.setString(4, user.getMemberName());
-            pstmt.setString(5, user.getPersonalNumber());
-            pstmt.setString(6, user.getTel());
-            pstmt.setString(7, user.getEmail());
-            pstmt.setString(8, user.getAddress());
-            pstmt.setString(9, user.getMemberPic());
-            pstmt.setString(10, user.getBackgroundPic());
-            pstmt.setString(11, user.getAllergy());
+        try (Connection conn = DBUtil.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
-            result = pstmt.executeUpdate();
+            pstmt.setString(1, dto.getMemberId());
+            pstmt.setString(2, dto.getPw());
+            pstmt.setString(3, dto.getMemberNickname());
+            pstmt.setString(4, dto.getMemberName());
+            pstmt.setString(5, dto.getPersonalNumber());
+            pstmt.setString(6, dto.getTel());
+            pstmt.setString(7, dto.getEmail());
+            pstmt.setString(8, dto.getAddress());
+            pstmt.setString(9, dto.getMemberPic());
+            pstmt.setString(10, dto.getBackgroundPic());
+            pstmt.setString(11, dto.getAllergy());
 
-        } catch (SQLException e) {
+            return pstmt.executeUpdate(); // 성공 시 1 반환
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
-        return result; // 성공 시 1, 실패 시 0
+        return 0; // 실패 시 0 반환
     }
 }
